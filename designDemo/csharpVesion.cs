eb030e66a6cc54b630554709b1b89c177ab584f7
liuliu01 <782202048@qq.com>
2017-06-14 09:17:36 +0800

    1.2

---
+++ designDemo/csharpVesion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designDemo
+//{
+{ 
+//    struct myData
+//    {
+//        fixed char love[123];
+//    }
+    class csharpVesion
+    {
+        char[] data = new char[100];
+        List<student> myClassMates = new List<student>();
+        
+        void get()
+        {
+            
+            var items = from c in
+                            from g in myClassMates
+                            group g by g.id
+                        select new { country = c.Key, name = c.Count() };
+
+
+        }
+    }
+   public class student
+    {
+        internal int id;
+        public string name;
+       internal int classId;
+    }
+}
